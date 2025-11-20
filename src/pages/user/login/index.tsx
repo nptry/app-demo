f96a7f44fd9bcc41dafd94927e137a3ecaff1149
司晓翰 <sixiaohan@sixiaohandeMac-mini.local>
2025-11-20 11:44:19 +0800

    更换logo

--- src/pages/user/login/index.tsx
+++ src/pages/user/login/index.tsx
@@ -182,7 +182,7 @@ const Login: React.FC = () => {
             minWidth: 280,
             maxWidth: '75vw',
           }}
-          logo={<img alt="logo" src="/logo.svg" />}
+          logo={<img alt="logo" src="/logo.png" />}
           title="Ant Design"
           subTitle={intl.formatMessage({
             id: 'pages.layouts.userLayout.title',
